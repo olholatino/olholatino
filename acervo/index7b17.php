3c1d9e8055b30f12eb286d9b350f49eb974df992
olholatino <olholatino@gmail.com>
2026-02-06 15:05:37 -0300

    Add Acervo

---
+++ acervo/index7b17.php
@@ -0,0 +1,56 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!-- generator="Joomla! - Open Source Content Management" -->
+<feed xmlns="http://www.w3.org/2005/Atom"  xml:lang="pt-br">
+	<title type="text">Acervo Olho Latino</title>
+	<subtitle type="text"></subtitle>
+	<link rel="alternate" type="text/html" href="http://localhost"/>
+	<id>http://localhost/acervo/index.php</id>
+	<updated>2026-02-05T22:40:43+00:00</updated>
+	<generator uri="http://joomla.org" version="2.5">Joomla! - Open Source Content Management</generator>
+	<link rel="self" type="application/atom+xml" href="http://localhost/acervo/index.php?format=feed&amp;type=atom"/>
+	<entry>
+		<title>Apresentação</title>
+		<link rel="alternate" type="text/html" href="http://localhost/acervo/index.php/9-uncategorised/531-apresentacao"/>
+		<published>2015-03-06T08:28:45+00:00</published>
+		<updated>2015-03-06T08:28:45+00:00</updated>
+		<id>http://localhost/acervo/index.php/9-uncategorised/531-apresentacao</id>
+		<author>
+			<name>Super User</name>
+			<email>lusans@gmail.com</email>
+		</author>
+		<summary type="html">&lt;div class=&quot;feed-description&quot;&gt;&lt;div&gt;&amp;nbsp;&lt;/div&gt;
+&lt;div&gt;É com imensa satisfação que apresentamos o site do Acervo Nacional do Museu Olho Latino, contando com 300 gravuras de 133 artistas, contendo fotos das obras e informações curriculares de seus autores. Apresentamos também o Catálogo Digital da Gravura Brasileira do Acervo Olho Latino, versão 2014, que, certamente, dará impulso para que novas versões aconteçam, conforme a inserção de novas gravuras à coleção. Para ficar mais acessível e com mais facilidade de se ler e apreciar o catálogo, optamos por colocar uma imagem de uma gravura de cada artista e no final a listagem geral das obras.&lt;/div&gt;
+&lt;div&gt;&amp;nbsp;&lt;/div&gt;
+&lt;div&gt;Gostaríamos que os artistas que estão com obras de sua autoria no nosso acervo, mantenham contato, enviando novas informações sobre suas exposições e a carreira artística, mantendo-nos atualizados também em relação ao endereço domiciliar e dados de contato. Para isso, o site disponibiliza um e-mail específico sobre o assunto.&lt;/div&gt;
+&lt;div&gt;&amp;nbsp;&lt;/div&gt;
+&lt;div&gt;Observamos que colocamos as gravuras dos artistas que estão devidamente inseridos em nossa coleção e, aqueles que, porventura, tenham obras sob os cuidados do Museu Olho Latino, mas não tenham assinado o termo de doação ainda ou não vejam suas obras, entrem em contato para que possamos oficializar a inserção delas em nossa coleção.&lt;/div&gt;
+&lt;div&gt;&amp;nbsp;&lt;/div&gt;
+&lt;div&gt;Agradecemos especialmente ao Programa de Ação Cultural – ProAC, da Secretaria da Cultura do Governo do Estado de São Paulo, que muito tem incentivado e valorizado a cultura, pela possibilidade de realização do Projeto Catálogo Digital da Gravura Brasileira do Acervo Olho Latino, contemplado com o seu apoio conforme o Edital nº 25/2013 do Programa de Ação Cultural – “Concurso de Apoio a Projetos de Difusão de Acervos Museológicos no Estado de São Paulo”.&lt;/div&gt;
+&lt;div&gt;&amp;nbsp;&lt;/div&gt;
+&lt;div&gt;Desejamos que os apreciadores da arte da gravura adquiram o hábito de visitar os nossos sites que envolvem a atuação do Olho Latino.&lt;/div&gt;
+&lt;div&gt;&amp;nbsp;&lt;/div&gt;
+&lt;div&gt;Agradecemos a atenção e venham visitar o setor expositivo do Museu Olho Latino localizado na Al. Lucas Nogueira Garcez, 511 - Parque das Águas – em Atibaia, SP.&lt;/div&gt;
+&lt;div&gt;&amp;nbsp;&lt;/div&gt;
+&lt;div style=&quot;text-align: right;&quot;&gt;Paulo Cheida Sans&lt;/div&gt;
+&lt;div style=&quot;text-align: right;&quot;&gt;Curador do Acervo Olho Latino&lt;/div&gt;
+&lt;div&gt;&amp;nbsp;&lt;/div&gt;&lt;/div&gt;</summary>
+		<content type="html">&lt;div class=&quot;feed-description&quot;&gt;&lt;div&gt;&amp;nbsp;&lt;/div&gt;
+&lt;div&gt;É com imensa satisfação que apresentamos o site do Acervo Nacional do Museu Olho Latino, contando com 300 gravuras de 133 artistas, contendo fotos das obras e informações curriculares de seus autores. Apresentamos também o Catálogo Digital da Gravura Brasileira do Acervo Olho Latino, versão 2014, que, certamente, dará impulso para que novas versões aconteçam, conforme a inserção de novas gravuras à coleção. Para ficar mais acessível e com mais facilidade de se ler e apreciar o catálogo, optamos por colocar uma imagem de uma gravura de cada artista e no final a listagem geral das obras.&lt;/div&gt;
+&lt;div&gt;&amp;nbsp;&lt;/div&gt;
+&lt;div&gt;Gostaríamos que os artistas que estão com obras de sua autoria no nosso acervo, mantenham contato, enviando novas informações sobre suas exposições e a carreira artística, mantendo-nos atualizados também em relação ao endereço domiciliar e dados de contato. Para isso, o site disponibiliza um e-mail específico sobre o assunto.&lt;/div&gt;
+&lt;div&gt;&amp;nbsp;&lt;/div&gt;
+&lt;div&gt;Observamos que colocamos as gravuras dos artistas que estão devidamente inseridos em nossa coleção e, aqueles que, porventura, tenham obras sob os cuidados do Museu Olho Latino, mas não tenham assinado o termo de doação ainda ou não vejam suas obras, entrem em contato para que possamos oficializar a inserção delas em nossa coleção.&lt;/div&gt;
+&lt;div&gt;&amp;nbsp;&lt;/div&gt;
+&lt;div&gt;Agradecemos especialmente ao Programa de Ação Cultural – ProAC, da Secretaria da Cultura do Governo do Estado de São Paulo, que muito tem incentivado e valorizado a cultura, pela possibilidade de realização do Projeto Catálogo Digital da Gravura Brasileira do Acervo Olho Latino, contemplado com o seu apoio conforme o Edital nº 25/2013 do Programa de Ação Cultural – “Concurso de Apoio a Projetos de Difusão de Acervos Museológicos no Estado de São Paulo”.&lt;/div&gt;
+&lt;div&gt;&amp;nbsp;&lt;/div&gt;
+&lt;div&gt;Desejamos que os apreciadores da arte da gravura adquiram o hábito de visitar os nossos sites que envolvem a atuação do Olho Latino.&lt;/div&gt;
+&lt;div&gt;&amp;nbsp;&lt;/div&gt;
+&lt;div&gt;Agradecemos a atenção e venham visitar o setor expositivo do Museu Olho Latino localizado na Al. Lucas Nogueira Garcez, 511 - Parque das Águas – em Atibaia, SP.&lt;/div&gt;
+&lt;div&gt;&amp;nbsp;&lt;/div&gt;
+&lt;div style=&quot;text-align: right;&quot;&gt;Paulo Cheida Sans&lt;/div&gt;
+&lt;div style=&quot;text-align: right;&quot;&gt;Curador do Acervo Olho Latino&lt;/div&gt;
+&lt;div&gt;&amp;nbsp;&lt;/div&gt;&lt;/div&gt;</content>
+		<category term="Destaque" />
+		<category term="Uncategorised" />
+	</entry>
+</feed>
---
+++ acervo/indexc0d0.php
@@ -0,0 +1,38 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!-- generator="Joomla! - Open Source Content Management" -->
+<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
+	<channel>
+		<title>Acervo Olho Latino</title>
+		<description><![CDATA[]]></description>
+		<link>http://localhost/acervo/index.php/component/content/?view=featured</link>
+		<lastBuildDate>Thu, 05 Feb 2026 22:40:42 +0000</lastBuildDate>
+		<generator>Joomla! - Open Source Content Management</generator>
+		<atom:link rel="self" type="application/rss+xml" href="http://localhost/acervo/index.php?format=feed&amp;type=rss"/>
+		<language>pt-br</language>
+		<item>
+			<title>Apresentação</title>
+			<link>http://localhost/acervo/index.php/9-uncategorised/531-apresentacao</link>
+			<guid isPermaLink="true">http://localhost/acervo/index.php/9-uncategorised/531-apresentacao</guid>
+			<description><![CDATA[<div class="feed-description"><div>&nbsp;</div>
+<div>É com imensa satisfação que apresentamos o site do Acervo Nacional do Museu Olho Latino, contando com 300 gravuras de 133 artistas, contendo fotos das obras e informações curriculares de seus autores. Apresentamos também o Catálogo Digital da Gravura Brasileira do Acervo Olho Latino, versão 2014, que, certamente, dará impulso para que novas versões aconteçam, conforme a inserção de novas gravuras à coleção. Para ficar mais acessível e com mais facilidade de se ler e apreciar o catálogo, optamos por colocar uma imagem de uma gravura de cada artista e no final a listagem geral das obras.</div>
+<div>&nbsp;</div>
+<div>Gostaríamos que os artistas que estão com obras de sua autoria no nosso acervo, mantenham contato, enviando novas informações sobre suas exposições e a carreira artística, mantendo-nos atualizados também em relação ao endereço domiciliar e dados de contato. Para isso, o site disponibiliza um e-mail específico sobre o assunto.</div>
+<div>&nbsp;</div>
+<div>Observamos que colocamos as gravuras dos artistas que estão devidamente inseridos em nossa coleção e, aqueles que, porventura, tenham obras sob os cuidados do Museu Olho Latino, mas não tenham assinado o termo de doação ainda ou não vejam suas obras, entrem em contato para que possamos oficializar a inserção delas em nossa coleção.</div>
+<div>&nbsp;</div>
+<div>Agradecemos especialmente ao Programa de Ação Cultural – ProAC, da Secretaria da Cultura do Governo do Estado de São Paulo, que muito tem incentivado e valorizado a cultura, pela possibilidade de realização do Projeto Catálogo Digital da Gravura Brasileira do Acervo Olho Latino, contemplado com o seu apoio conforme o Edital nº 25/2013 do Programa de Ação Cultural – “Concurso de Apoio a Projetos de Difusão de Acervos Museológicos no Estado de São Paulo”.</div>
+<div>&nbsp;</div>
+<div>Desejamos que os apreciadores da arte da gravura adquiram o hábito de visitar os nossos sites que envolvem a atuação do Olho Latino.</div>
+<div>&nbsp;</div>
+<div>Agradecemos a atenção e venham visitar o setor expositivo do Museu Olho Latino localizado na Al. Lucas Nogueira Garcez, 511 - Parque das Águas – em Atibaia, SP.</div>
+<div>&nbsp;</div>
+<div style="text-align: right;">Paulo Cheida Sans</div>
+<div style="text-align: right;">Curador do Acervo Olho Latino</div>
+<div>&nbsp;</div></div>]]></description>
+			<author>lusans@gmail.com (Super User)</author>
+			<category>Destaque</category>
+			<category>Uncategorised</category>
+			<pubDate>Fri, 06 Mar 2015 08:28:45 +0000</pubDate>
+		</item>
+	</channel>
+</rss>
