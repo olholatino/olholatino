f85dcad96ca45124194ba3b85ea096a8c010022f
olholatino <olholatino@gmail.com>
2026-02-08 04:08:04 -0300

    Atualizacao links Site Museu 2

--- site/indexc0d0.php
+++ site/indexc0d0.php
@@ -12,32 +12,32 @@
 			<title>Acontece</title>
 			<link>http://localhost/site/index.php?option=com_content&amp;view=article&amp;id=52:acontece&amp;catid=40:pagina-principal</link>
 			<guid>http://localhost/site/index.php?option=com_content&amp;view=article&amp;id=52:acontece&amp;catid=40:pagina-principal</guid>
-			<description><![CDATA[<div style="text-align: center;"><a style="color: #23803f; text-align: center;" href="index.php/acontece/281-museu-olho-latino-expoe-xilogravuras-de-helio-schonmann" target="_blank"><img src="http://olholatino.com.br/media/rokgallery/a/a2a334a2-6b1e-4acb-8739-cd049e08bd3f/069a0d60-2981-46b9-d8f4-7c6198395f91.jpg" alt="helio schonmann museu olho latino gravura curadoria paulo cheida sans" width="150" /></a></div>
+			<description><![CDATA[<div style="text-align: center;"><a style="color: #23803f; text-align: center;" href="../index.php/acontece/281-museu-olho-latino-expoe-xilogravuras-de-helio-schonmann" target="_blank"><img src="http://olholatino.com.br/media/rokgallery/a/a2a334a2-6b1e-4acb-8739-cd049e08bd3f/069a0d60-2981-46b9-d8f4-7c6198395f91.jpg" alt="helio schonmann museu olho latino gravura curadoria paulo cheida sans" width="150" /></a></div>
 <div><strong>Museu Olho Latino expõe xilogravuras de Hélio Schonmann</strong></div>
-<div><strong><a href="index.php/acontece/281-museu-olho-latino-expoe-xilogravuras-de-helio-schonmann">&nbsp;</a></strong></div>
+<div><strong><a href="../index.php/acontece/281-museu-olho-latino-expoe-xilogravuras-de-helio-schonmann">&nbsp;</a></strong></div>
 <div>A mostra “Xilogravuras” do artista plástico Hélio Schonmann está em exposição de 06 a 26 de outubro no Museu Olho Latino. A curadoria é do prof. Dr. Paulo Cheida Sans.</div>
-<div style="text-align: right;"><a href="index.php/acontece/281-museu-olho-latino-expoe-xilogravuras-de-helio-schonmann" target="_blank"><span style="font-size: 8pt;"><em>Leia Mais</em></span></a></div>
+<div style="text-align: right;"><a href="../index.php/acontece/281-museu-olho-latino-expoe-xilogravuras-de-helio-schonmann" target="_blank"><span style="font-size: 8pt;"><em>Leia Mais</em></span></a></div>
 <div style="text-align: center;">&nbsp;</div>
 <div style="text-align: center;"><hr />&nbsp;</div>
 <div style="text-align: center;">&nbsp;</div>
-<div style="text-align: center;">&nbsp;<a href="index.php/acontece/273-museu-olho-latino-expoe-xilogravuras-de-um-dos-principais-artistas-da-arte-moderna-brasileira-xico-stockinger" target="_blank"><img src="../media/rokgallery/4/49130c82-43f8-4615-dfea-f4c72d62f99a/7afd4473-5d1f-49d6-9119-ce8abc4fe23c.jpg" alt="francisco xico stockinger curadoria paulo cheida sans xilogravuras museu olho latino" width="150" /></a></div>
-<div><strong>Museu Olho Latino expõe xilogravuras de um dos principais artistas da Arte Moderna Brasileira<a href="index.php/acontece/273-museu-olho-latino-expoe-xilogravuras-de-um-dos-principais-artistas-da-arte-moderna-brasileira-xico-stockinger">&nbsp;</a></strong></div>
+<div style="text-align: center;">&nbsp;<a href="../index.php/acontece/273-museu-olho-latino-expoe-xilogravuras-de-um-dos-principais-artistas-da-arte-moderna-brasileira-xico-stockinger" target="_blank"><img src="../media/rokgallery/4/49130c82-43f8-4615-dfea-f4c72d62f99a/7afd4473-5d1f-49d6-9119-ce8abc4fe23c.jpg" alt="francisco xico stockinger curadoria paulo cheida sans xilogravuras museu olho latino" width="150" /></a></div>
+<div><strong>Museu Olho Latino expõe xilogravuras de um dos principais artistas da Arte Moderna Brasileira<a href="../index.php/acontece/273-museu-olho-latino-expoe-xilogravuras-de-um-dos-principais-artistas-da-arte-moderna-brasileira-xico-stockinger">&nbsp;</a></strong></div>
 <div>A mostra “Xilogravuras” do artista plástico Xico Stockinger, um dos principais artistas da Arte Moderna Brasileira, está em exposição de 04 de julho a 31 de agosto no Museu Olho Latino, em Atibaia, SP. A curadoria é do prof. Dr. Paulo Cheida Sans.</div>
-<div style="text-align: right;"><a href="index.php/acontece/273-museu-olho-latino-expoe-xilogravuras-de-um-dos-principais-artistas-da-arte-moderna-brasileira-xico-stockinger" target="_blank"><em><span style="font-size: 8pt;">Leia Mais</span></em></a></div>
+<div style="text-align: right;"><a href="../index.php/acontece/273-museu-olho-latino-expoe-xilogravuras-de-um-dos-principais-artistas-da-arte-moderna-brasileira-xico-stockinger" target="_blank"><em><span style="font-size: 8pt;">Leia Mais</span></em></a></div>
 <div>&nbsp;</div>
 <div style="text-align: center;"><hr />&nbsp;&nbsp;&nbsp;&nbsp;</div>
-<div style="text-align: center;"><a href="index.php/acontece/272-museu-olho-latino-comemora-15-anos-com-mostra-de-paulo-cheida-celina-carvalho-e-o-grupo-olho-latino" target="_blank"><img src="../media/rokgallery/4/45758e50-6c34-4590-efd6-b23c71afe551/46898ccd-5c3d-4e2f-99b8-1de292730415.jpg" alt="" width="150" /></a></div>
+<div style="text-align: center;"><a href="../index.php/acontece/272-museu-olho-latino-comemora-15-anos-com-mostra-de-paulo-cheida-celina-carvalho-e-o-grupo-olho-latino" target="_blank"><img src="../media/rokgallery/4/45758e50-6c34-4590-efd6-b23c71afe551/46898ccd-5c3d-4e2f-99b8-1de292730415.jpg" alt="" width="150" /></a></div>
 <div style="text-align: left;"><strong>Vídeo da abertura de "Focos Antológicos"</strong></div>
 <div>Confira o vídeo da abertura de "Focos Antológicos", um mega evento em comemoração aos 15 anos do Museu Olho Latino que está em exposição até 27 de março no MAC de Campinas. Aproveite e faça sua visita! *Estará aberto inclusive no domingo de Páscoa!</div>
-<div style="text-align: right;"><a href="index.php/acontece/272-museu-olho-latino-comemora-15-anos-com-mostra-de-paulo-cheida-celina-carvalho-e-o-grupo-olho-latino" target="_blank"><em><span style="font-size: 8pt;">Veja Mais</span></em></a></div>
+<div style="text-align: right;"><a href="../index.php/acontece/272-museu-olho-latino-comemora-15-anos-com-mostra-de-paulo-cheida-celina-carvalho-e-o-grupo-olho-latino" target="_blank"><em><span style="font-size: 8pt;">Veja Mais</span></em></a></div>
 <div style="text-align: center;">&nbsp;</div>
 <div style="text-align: center;"><hr /></div>
-<div style="text-align: center;">&nbsp;<a href="index.php/acontece/261-paulo-cheida-sans-lanca-livro-na-fnac-campinas" target="_blank">&nbsp;</a>&nbsp;<a href="index.php/acontece/261-paulo-cheida-sans-lanca-livro-na-fnac-campinas" target="_blank">&nbsp;</a></div>
+<div style="text-align: center;">&nbsp;<a href="../index.php/acontece/261-paulo-cheida-sans-lanca-livro-na-fnac-campinas" target="_blank">&nbsp;</a>&nbsp;<a href="../index.php/acontece/261-paulo-cheida-sans-lanca-livro-na-fnac-campinas" target="_blank">&nbsp;</a></div>
 <div style="text-align: center;">
-<div style="text-align: center;"><a href="index.php/acontece/268-focos-antologicos-olho-latino-comemora-15-anos-com-mega-exposicao-no-mac-campinas" target="_blank"><img src="../media/rokgallery/1/11ed5996-89df-49df-e16c-73c20a331e32/10413c7b-048a-4042-f8c3-58e1f36d4df1.png" alt="focos antologicos olho latino macc campinas" width="150" /></a></div>
+<div style="text-align: center;"><a href="../index.php/acontece/268-focos-antologicos-olho-latino-comemora-15-anos-com-mega-exposicao-no-mac-campinas" target="_blank"><img src="../media/rokgallery/1/11ed5996-89df-49df-e16c-73c20a331e32/10413c7b-048a-4042-f8c3-58e1f36d4df1.png" alt="focos antologicos olho latino macc campinas" width="150" /></a></div>
 <div style="text-align: left;"><strong>"Olho Latino: Focos Antológicos"</strong></div>
 <div style="text-align: left;">A mostra "Olho Latino: Focos Antológicos" abre no dia 18 de fevereiro, às 20h, no Museu de Arte Contemporânea de Campinas (MACC). É uma mega exposição que comemora os 15 anos de existência do Museu de Arte Contemporânea Olho Latino. Com curadoria do Prof. Dr. Paulo Cheida Sans, a mostra possui 3 momentos expositivos que abarcam a essência Olho Latino, expondo mais de 100 obras entre pinturas, gravuras, esculturas e instalações.</div>
-<div style="text-align: right;"><a href="index.php/acontece/268-focos-antologicos-olho-latino-comemora-15-anos-com-mega-exposicao-no-mac-campinas" target="_blank"><em><span style="font-size: 8pt;">Leia Mais</span></em></a></div>
+<div style="text-align: right;"><a href="../index.php/acontece/268-focos-antologicos-olho-latino-comemora-15-anos-com-mega-exposicao-no-mac-campinas" target="_blank"><em><span style="font-size: 8pt;">Leia Mais</span></em></a></div>
 <div style="text-align: right;"><em><span style="font-size: 8pt;"><span style="text-align: center;">&nbsp;</span></span></em></div>
 </div>]]></description>
 			<author>olholatino01@terra.com.br (Administrator)</author>
